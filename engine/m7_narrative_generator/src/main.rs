a23deebba094eee09cea26dc08c70e5e5b1c5796
alvrui <61255408+alvrui@users.noreply.github.com>
2026-07-01 14:58:28 +0200

    Fix main.rs imports to use crate paths

--- engine/m7_narrative_generator/src/main.rs
+++ engine/m7_narrative_generator/src/main.rs
@@ -1,24 +1,18 @@
-use std::path::Path;
-
-use m7_narrative_generator::chain_generator::{MotorContext, NarrativeChainGenerator};
-use m7_narrative_generator::sdk_config::SdkConfig;
+use crate::chain_generator::{MotorContext, NarrativeChainGenerator};
+use crate::sdk_config::SdkConfig;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Cargar configuración del SDK
     let config_dir = "../../config";
     let config = SdkConfig::load(config_dir)?;
     
     let generator = NarrativeChainGenerator::new(config);
     
-    // Cargar contexto del motor
     let context_path = format!("{}/SDK/motor2sdk.json", config_dir);
     let context = MotorContext::load(&context_path)?;
     
-    // Generar cadena narrativa
     let solicitud_id = "test_001";
     let chain = generator.generate(&context, solicitud_id)?;
     
-    // Guardar resultado
     let output_path = format!("{}/SDK/sdk2motor.json", config_dir);
     chain.save(&output_path)?;
     
