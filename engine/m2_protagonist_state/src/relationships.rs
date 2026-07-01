305a46b1fc599f2e9ba730267208d39e4dd0bef3
alvrui <61255408+alvrui@users.noreply.github.com>
2026-07-01 12:48:46 +0200

    Extend Relationships to support NPCs

--- engine/m2_protagonist_state/src/relationships.rs
+++ engine/m2_protagonist_state/src/relationships.rs
@@ -2,11 +2,15 @@ use crate::error::ProtagonistError;
 use crate::models::Relationships;
 
 impl Relationships {
-    pub fn add_relationship(&mut self, faction_id: String, value: i32) {
+    pub fn add_faction_relationship(&mut self, faction_id: String, value: i32) {
         self.factions.insert(faction_id, value);
     }
 
-    pub fn update_relationship(
+    pub fn add_npc_relationship(&mut self, npc_id: String, value: i32) {
+        self.npcs.insert(npc_id, value);
+    }
+
+    pub fn update_faction_relationship(
         &mut self,
         faction_id: &str,
         value: i32,
@@ -19,7 +23,20 @@ impl Relationships {
         }
     }
 
-    pub fn increase_relationship(
+    pub fn update_npc_relationship(
+        &mut self,
+        npc_id: &str,
+        value: i32,
+    ) -> Result<(), ProtagonistError> {
+        if self.npcs.contains_key(npc_id) {
+            self.npcs.insert(npc_id.to_string(), value);
+            Ok(())
+        } else {
+            Err(ProtagonistError::RelationshipNotFound(npc_id.to_string()))
+        }
+    }
+
+    pub fn increase_faction_relationship(
         &mut self,
         faction_id: &str,
         amount: i32,
@@ -32,19 +49,48 @@ impl Relationships {
         }
     }
 
-    pub fn get_relationship(&self, faction_id: &str) -> Result<i32, ProtagonistError> {
+    pub fn increase_npc_relationship(
+        &mut self,
+        npc_id: &str,
+        amount: i32,
+    ) -> Result<(), ProtagonistError> {
+        if let Some(current) = self.npcs.get_mut(npc_id) {
+            *current += amount;
+            Ok(())
+        } else {
+            Err(ProtagonistError::RelationshipNotFound(npc_id.to_string()))
+        }
+    }
+
+    pub fn get_faction_relationship(&self, faction_id: &str) -> Result<i32, ProtagonistError> {
         self.factions
             .get(faction_id)
             .copied()
             .ok_or_else(|| ProtagonistError::RelationshipNotFound(faction_id.to_string()))
     }
 
-    pub fn remove_relationship(&mut self, faction_id: &str) -> Result<(), ProtagonistError> {
+    pub fn get_npc_relationship(&self, npc_id: &str) -> Result<i32, ProtagonistError> {
+        self.npcs
+            .get(npc_id)
+            .copied()
+            .ok_or_else(|| ProtagonistError::RelationshipNotFound(npc_id.to_string()))
+    }
+
+    pub fn remove_faction_relationship(&mut self, faction_id: &str) -> Result<(), ProtagonistError> {
         if self.factions.contains_key(faction_id) {
             self.factions.remove(faction_id);
             Ok(())
         } else {
             Err(ProtagonistError::RelationshipNotFound(faction_id.to_string()))
+        }
+    }
+
+    pub fn remove_npc_relationship(&mut self, npc_id: &str) -> Result<(), ProtagonistError> {
+        if self.npcs.contains_key(npc_id) {
+            self.npcs.remove(npc_id);
+            Ok(())
+        } else {
+            Err(ProtagonistError::RelationshipNotFound(npc_id.to_string()))
         }
     }
 }
