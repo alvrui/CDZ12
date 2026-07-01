3b311188e921fd6e788613847c5f7f0ba6207dce
alvrui <61255408+alvrui@users.noreply.github.com>
2026-07-01 14:58:31 +0200

    Add sdk_config to lib.rs exports

--- engine/m7_narrative_generator/src/lib.rs
+++ engine/m7_narrative_generator/src/lib.rs
@@ -1,13 +1,17 @@
+pub mod chain_generator;
 pub mod config;
 pub mod error;
 pub mod generator;
 pub mod models;
+pub mod sdk_config;
 pub mod serialization;
 pub mod triggers;
 
-pub use config::ConfigManager;
-pub use error::NarrativeError;
-pub use generator::NarrativeGenerator;
+pub use chain_generator::*;
+pub use config::*;
+pub use error::*;
+pub use generator::*;
 pub use models::*;
+pub use sdk_config::*;
 pub use serialization::*;
-pub use triggers::TriggerStore;
+pub use triggers::*;
